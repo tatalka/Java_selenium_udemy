6fa8cadbd3b493f71ed5058c5f9d164cc8dc21e4
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-02-06 22:23:11 +0000

    Szkielet pierwszy

---
+++ src/testPackage/FirstTestClass.java
@@ -0,0 +1,14 @@
+package testPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by olewc on 06/02/2017.
+ */
+public class FirstTestClass {
+
+    public static void main(String[] args){
+        WebDriver driver = new FirefoxDriver();
+    }
+}
