417f2883a3653f0a9a90ff6a3196802ffc897440
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-02-08 22:06:52 +0000

    filmik 49 praca z selenium 3.x

--- src/testPackage/FirstTestClass.java
+++ src/testPackage/FirstTestClass.java
@@ -1,14 +1,28 @@
 package testPackage;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+
 
 /**
  * Created by olewc on 06/02/2017.
  */
 public class FirstTestClass {
 
-    public static void main(String[] args){
-        WebDriver driver = new FirefoxDriver();
+    public static void main(String[] args) {
+        String baseURL = "http://www.onet.pl";
+        System.setProperty("webdriver.gecko.driver","E:\\developernia\\selenium-java-3.0.1\\geckodriver.exe");
+//        WebDriver driver = new FirefoxDriver();
+//        driver.get(baseURL);
+//        driver.close();
+        System.setProperty("webdriver.chrome.driver","E:\\developernia\\selenium-java-3.0.1\\chromedriver.exe");
+        WebDriver chromeDriver = new ChromeDriver();
+
+        chromeDriver.get(baseURL);
+        
+
     }
 }
+
