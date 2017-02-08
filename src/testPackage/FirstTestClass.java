9d1ea2bbae4aadab319d6575c65a3714797e008b
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-02-08 22:59:35 +0000

    filmik 52 - selenium w Internet Explorer 11

--- src/testPackage/FirstTestClass.java
+++ src/testPackage/FirstTestClass.java
@@ -1,20 +1,17 @@
 package testPackage;
 
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class FirstTestClass {
 
     public static void main(String[] args) {
-    // tak sie wlacza przegladarke chrome
-        System.setProperty("webdriver.chrome.driver", "E:\\developernia\\selenium-java-3.0.1\\chromedriver.exe");
+    // tak sie wlacza przegladarke internet explorer
+        System.setProperty("webdriver.ie.driver", "E:\\developernia\\selenium-java-3.0.1\\IEDriverServer.exe");
         String baseURL = "http://www.onet.pl";
         WebDriver driver;
 
-        driver = new ChromeDriver();
+        driver = new InternetExplorerDriver();
         driver.manage().window().maximize();
         driver.get(baseURL);
 
