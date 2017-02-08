b6fe344a3623ec069c7e2fb2025e1a33772cabb8
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-02-08 22:38:45 +0000

    filmik 51 - uzycie selenium w przegladarce chrome

--- src/testPackage/FirstTestClass.java
+++ src/testPackage/FirstTestClass.java
@@ -2,27 +2,22 @@ package testPackage;
 
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class FirstTestClass {
 
     public static void main(String[] args) {
-    // pokazanie w działaniu klasy DesiredCapabilities
-        System.setProperty("webdriver.gecko.driver","E:\\developernia\\selenium-java-3.0.1\\geckodriver.exe");
-
+    // tak sie wlacza przegladarke chrome
+        System.setProperty("webdriver.chrome.driver", "E:\\developernia\\selenium-java-3.0.1\\chromedriver.exe");
         String baseURL = "http://www.onet.pl";
         WebDriver driver;
-        DesiredCapabilities caps = DesiredCapabilities.firefox();
 
-        caps.setBrowserName("Firefox");
-        caps.setPlatform(Platform.WIN10);
-
-        driver = new FirefoxDriver(caps);
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(baseURL);
 
-        // za cholerę na razie nie wiem po co ta klasa, podobno jest w użyciu przy okazji grida. 
     }
 }
 
