87fcff02538611718b5a7aab66a82694c4ab65d2
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-02-13 21:21:45 +0000

    filmik 58 wyszukiwanie po czesciowym i calym stringu w anchor text.

--- src/testPackage/FirstTestClass.java
+++ src/testPackage/FirstTestClass.java
@@ -1,5 +1,6 @@
 package testPackage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -7,13 +8,16 @@ public class FirstTestClass {
 
     public static void main(String[] args) {
 
-        //System.setProperty("webdriver.gecko.driver","C:\\olew\\selenium_drivers\\geckodriver.exe");
-        String baseURL = "http://www.google.com";
-        WebDriver driver;
-
-        driver = new FirefoxDriver();
+        String baseURL = "http://demostore.x-cart.com";
+        WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.get(baseURL);
+        // ten znajduje wylacznie caly tekst:
+        driver.findElement(By.linkText("Contact us")).click();
+        // a temu wystarczy kawalek:
+        driver.findElement(By.partialLinkText("Contact")).click();
+        // na tej stronie z kursu cwiczenie nie dziala - prawdopodobnie
+        // dlatego, ze tekst nie jest w "anchor" tylko w "span"
 
     }
 }
