7e891bd9996b752f7cc70295478003beff5257c4
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-02-13 20:51:48 +0000

    plik *.iml do ignora, sa rozne sciezki na roznych kompach, tego nie trzea sledzic.

--- src/testPackage/FirstTestClass.java
+++ src/testPackage/FirstTestClass.java
@@ -7,12 +7,12 @@ public class FirstTestClass {
 
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.gecko.driver","C:\\olew\\selenium_drivers\\geckodriver.exe");
+        //System.setProperty("webdriver.gecko.driver","C:\\olew\\selenium_drivers\\geckodriver.exe");
         String baseURL = "http://www.google.com";
         WebDriver driver;
 
         driver = new FirefoxDriver();
-        //driver.manage().window().maximize();
+        driver.manage().window().maximize();
         driver.get(baseURL);
 
     }
