c6c9ac0df8a7a09714b25b07856546ad1ff7944c
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-02-13 23:16:54 +0000

    filmik 61 - szukanie po kaskadowych arkuszach stylow - CSS

--- src/testPackage/FirstTestClass.java
+++ src/testPackage/FirstTestClass.java
@@ -15,6 +15,22 @@ public class FirstTestClass {
         // szukanie elementu po nazwie tagu. praktycznie nie uzywane, bo tagi nie sa unikatowe
         // wiec albo sie wywali, albo kliknie w miejsce ktorego nie mozna byc pewnym
         driver.findElement(By.tagName("a")).click();
+
+        // znajdowanie elementu po selektorze CSS
+        /*
+        teoretycznie id powinny byc unikatowe, ale zdarza sie ze nie jest
+        wtedy mozna :
+        1) input[id=displayed-text]  -> najdluzszy sposob. nazwa tagu, id
+        2) #displayed-text   -> znajdzie wszystkie elementy z takim id, bo # to wlasnie id.
+            oczywiscie wywali sie, bo selenium zglupieje jak nie bedzie jednego elementu
+        3) input#displayed-text
+
+        innym znakiem specjalnym jest "." czyli kropka, tak jak # oznaczal id, tak '.' oznacza klase.
+
+        .displayed-class
+        input.displayed-class
+         */
+
     }
 }
 
