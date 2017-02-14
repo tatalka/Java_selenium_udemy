261ccac0957aa0e8a61cbda82523f80d5a70eb9a
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-02-14 22:14:57 +0000

    filmik 64 - finding child elements

--- src/testPackage/FirstTestClass.java
+++ src/testPackage/FirstTestClass.java
@@ -13,20 +13,10 @@ public class FirstTestClass {
         driver.manage().window().maximize();
         driver.get(baseURL);
 
-        // wildcards w wyszukiwaniu:
-        // "^"  -> represents starting text (starts with)
-        // tutaj po dostawieniu daszka znajdzie dwa takie elementy - jeden mial klase inputs
-        // a drugi dwie klasy :  inputs i displayed-class
-        // driver.findElement(By.cssSelector("input[class^='inputs']")).sendKeys("jablko");
-        // "$"  -> represents ending text
-        // w przykladzie ponizszym bez dolara nie znalazly tego elementu, poniewaz ma on rowniez
-        // klase 'inputs'. Dolar jako wildcard zalatwia sprawe bo mowi - klasy koncza sie na [blabla]
-        // po dolarze nie musi byc kompletna nazwa klasy, wystarczy kawalek. BTW - to moze byc dowolny
-        // atrybut, nie musi byc klasa, tez bedzie dzialac.
-        // driver.findElement(By.cssSelector("input[class='displayed-class'")).sendKeys("gruszka");
-        // "*"  -> represents the text contained
-        // gwiazdka pozwala na znalezienie atrybutu ktorego wartosc po prostu zawiera wyslany string:
-        driver.findElement(By.cssSelector("input[placeholder*='Enter'")).sendKeys("pomarancza");
+        // finding children elements
+        // '>' wskazuje na child element
+        driver.findElement(By.cssSelector("fieldset>input#displayed-text")).sendKeys("jablko");
+
 
 
     }
