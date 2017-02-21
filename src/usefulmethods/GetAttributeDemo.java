4ab3e8d4a7ac218b41ed6a908622b2072da29ed0
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-02-21 23:47:23 +0000

    filmik 86. how to get value of element attribute

---
+++ src/usefulmethods/GetAttributeDemo.java
@@ -0,0 +1,47 @@
+package usefulmethods;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by olewc on 21/02/2017.
+ */
+public class GetAttributeDemo {
+
+    private WebDriver driver;
+    private String baseUrl;
+
+    @Before
+    public void setUp() throws Exception{
+        driver = new FirefoxDriver();
+        baseUrl = "https://letskodeit.teachable.com/pages/practice";
+
+        // maximize browser window and setup timeout
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testGetText() throws Exception{
+
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.id("name"));
+        String attributeValue = element.getAttribute("type");
+
+
+        System.out.println("Value of attribute is : " + attributeValue);
+    }
+
+    @After
+    public void tearDown() throws Exception{
+
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
