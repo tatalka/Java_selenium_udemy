67e763e857268a61cfb174399f51ddb3fec82d5f
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-04-23 13:12:19 +0100

    Sekcja 6 video 50 -> Użycie klasy DesiredCapabilities do inicjalizacji webdrivera.

--- src/testPackage/FirstTestClass.java
+++ src/testPackage/FirstTestClass.java
@@ -1,7 +1,9 @@
 package testPackage;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class FirstTestClass {
 
@@ -12,11 +14,18 @@ public class FirstTestClass {
         // property musi być ustawione ZANIM zainicjalizujemy driver.
         String baseURL = "http://www.onet.pl";
 
-        // mozna już zainicjalizować drivera tutaj:
-        driver = new FirefoxDriver();
-        driver.get(baseURL);
+        // w tym filmiku pokazują sposób użycia DesiredCapabilities:
+        DesiredCapabilities caps = DesiredCapabilities.firefox(); // tutaj ustawiamy ze chcemy kfirefoxa
+        caps.setPlatform(Platform.WIN10);
+        caps.setBrowserName("firefox");
 
-        // driver.close(); // to polecenie nie działa. nie wiem czemu, quit tak.
+        // nowy sposób inicjalizacji webdrivera, używa ustawień zdefiniowanych w klasie DesiredCapabilities
+        // póki co ustawienia te nie mają znaczenia, jako platforme można podać coś innego, ale będzie to miało znaczenie
+        // jak zacznie się używać grida.
+        driver = new FirefoxDriver(caps);
+        driver.get(baseURL);
+        driver.manage().window().maximize();
+
         driver.quit();
     }
 }
