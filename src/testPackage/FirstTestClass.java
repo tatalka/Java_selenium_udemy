d6eae9e90dc75aa0ca7294e24641da3bbd03660e
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-04-23 14:20:48 +0100

    Sekcja 6 video 52 -> uruchamianie selenium na IE 11

--- src/testPackage/FirstTestClass.java
+++ src/testPackage/FirstTestClass.java
@@ -1,19 +1,22 @@
 package testPackage;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class FirstTestClass {
 
     public static void main(String[] args) {
-    // selenium w chrome
+    // uruchomienie testow selenium w IE 11
         String baseURL = "http://www.google.com";
-
         WebDriver driver;
-        System.setProperty("webdriver.chrome.driver","E:\\developernia\\selenium-java-3.0.1\\chromedriver.exe");
-        driver = new ChromeDriver();
+
+        // dodanie do zmiennej systemowej path sciezki do katalogu z driverami
+        // powoduje ze juz nie trzeba ustawiac tej zmiennej ponizje - bedzie dzialac tak czy siak.
+        System.setProperty("webdriver.ie.driver","E:\\developernia\\selenium-java-3.0.1\\IEDriverServer.exe");
+        driver = new InternetExplorerDriver();
 
         driver.get(baseURL);
+
 
         driver.quit();
     }
