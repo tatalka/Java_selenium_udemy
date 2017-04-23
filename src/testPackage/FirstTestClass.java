443acd8b5f6ac338d08ab7a8b58295463ca14c2b
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-04-23 12:54:07 +0100

    Sekcja 6 video 49 -> ustawianie System property dla gecko. To trzeba ustawiać dla firefoxa dla selenium 3.1 i nowszych - zmienił się webdriver.

--- src/testPackage/FirstTestClass.java
+++ src/testPackage/FirstTestClass.java
@@ -1,30 +1,23 @@
 package testPackage;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-
-
-/**
- * Created by olewc on 06/02/2017.
- */
 public class FirstTestClass {
 
     public static void main(String[] args) {
+        WebDriver driver;
+        System.setProperty("webdriver.gecko.driver","E:\\developernia\\selenium-java-3.0.1\\geckodriver.exe");
+        // od Selenium 3.1 i nowszych trzeba ustawiać właściwości tak jak wyżej, więc poprzednie wersje tego nie potrzebowały
+        // property musi być ustawione ZANIM zainicjalizujemy driver.
         String baseURL = "http://www.onet.pl";
-        // System.setProperty("webdriver.gecko.driver","E:\\developernia\\selenium-java-3.0.1\\geckodriver.exe");
-        // nie wiem co to to za linijka powyżej, skomentuję jak bedzie koleś o tym gadać później.
-        WebDriver driver = new FirefoxDriver();
+
+        // mozna już zainicjalizować drivera tutaj:
+        driver = new FirefoxDriver();
         driver.get(baseURL);
-        // driver.close(); // to polecenie nie działa.
+
+        // driver.close(); // to polecenie nie działa. nie wiem czemu, quit tak.
         driver.quit();
-        //System.setProperty("webdriver.chrome.driver","E:\\developernia\\selenium-java-3.0.1\\chromedriver.exe");
-        //WebDriver chromeDriver = new ChromeDriver();
-
-        //chromeDriver.get(baseURL);
-        
-
     }
 }
 
