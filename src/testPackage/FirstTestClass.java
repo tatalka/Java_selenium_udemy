41d99e7bfdadfbca9c71c7d872a14ea2fbb50bd3
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-04-23 15:43:24 +0100

    Sekcja 6 video 56 -> znajdowanie elementu za pomocą id albo Xpath

--- src/testPackage/FirstTestClass.java
+++ src/testPackage/FirstTestClass.java
@@ -1,24 +1,33 @@
 package testPackage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstTestClass {
 
     public static void main(String[] args) {
-    // uruchomienie testow selenium w IE 11
-        String baseURL = "http://www.google.com";
         WebDriver driver;
+        String baseURL = "http://www.google.com";
 
-        // dodanie do zmiennej systemowej path sciezki do katalogu z driverami
-        // powoduje ze juz nie trzeba ustawiac tej zmiennej ponizje - bedzie dzialac tak czy siak.
-        System.setProperty("webdriver.ie.driver","E:\\developernia\\selenium-java-3.0.1\\IEDriverServer.exe");
-        driver = new InternetExplorerDriver();
-
+        //System.setProperty("webdriver.firefox.driver","E:\\developernia\\selenium-java-3.0.1\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
         driver.get(baseURL);
 
+        // znajdz element o takim id i wyslij do nie go tekst
+        // jezeli pojawi się ten błąd podczas kompilacji :
+        //Error:(20, 54) java: incompatible types: java.lang.String cannot be converted to java.lang.CharSequence[]
+        // to we właściwościach projektu trzeba przestawić poziom języka na 1.8 (albo i nowszy) i bedzie ok.
+        driver.findElement(By.id("lst-ib")).sendKeys("poziomka");
 
-        driver.quit();
+        // tutaj albo mogę znowu znaleźć guzik po id :
+        driver.findElement(By.id("_fZl")).click();
+
+        // albo znaleźć jego xpath (czasami nie ma id, albo po prostu łatwiej:
+        //driver.findElement(By.xpath(".//*[@id='_fZl']")).click();
+
+        //driver.quit(); // zakomentowałem bo za szybko działa i od razu zamyka
     }
 }
 
