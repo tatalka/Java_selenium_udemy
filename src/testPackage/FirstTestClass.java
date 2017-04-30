25b4a1a67c29aa59c2c92e3c508ce846d277a804
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-04-30 22:40:08 +0100

    Section 8 video 62 -> finding elements using css class name

--- src/testPackage/FirstTestClass.java
+++ src/testPackage/FirstTestClass.java
@@ -8,13 +8,14 @@ public class FirstTestClass {
 
     public static void main(String[] args) {
         WebDriver driver;
-        String baseURL = "http://demostore.x-cart.com/";
+        String baseURL = "http://letskodeit.teachable.com/pages/practice/";
 
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.get(baseURL);
 
-        driver.findElement(By.tagName("a")).click();
+        driver.findElement(By.cssSelector(".displayed-class")).click();
+
 
         //driver.quit();
     }
