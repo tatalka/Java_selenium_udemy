4f0ee62f6cc36e75d519b32aa08ef65357d7da2a
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-04-30 14:45:51 +0100

    Sekcja 6 video 58 - znajdowanie elementów po kawałku tekstu linka (partial text)

--- src/testPackage/FirstTestClass.java
+++ src/testPackage/FirstTestClass.java
@@ -8,29 +8,19 @@ public class FirstTestClass {
 
     public static void main(String[] args) {
         WebDriver driver;
-        String baseURL = "http://www.google.com";
+        String baseURL = "https://pl.ogame.gameforge.com/";
 
-        //System.setProperty("webdriver.firefox.driver","E:\\developernia\\selenium-java-3.0.1\\geckodriver.exe");
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.get(baseURL);
 
-        // znajdz element o takim id i wyslij do nie go tekst
-        // jezeli pojawi się ten błąd podczas kompilacji :
-        //Error:(20, 54) java: incompatible types: java.lang.String cannot be converted to java.lang.CharSequence[]
-        // to we właściwościach projektu trzeba przestawić poziom języka na 1.8 (albo i nowszy) i bedzie ok.
-        driver.findElement(By.id("lst-ib")).sendKeys("poziomka");
+        // finding element by full text
+        //driver.findElement(By.linkText("Oświadczanie o ochronie danych")).click();
+        // finding element by partial text
+        driver.findElement(By.partialLinkText("Oświadczanie o ")).click();
 
-        // tutaj albo mogę znowu znaleźć guzik po id :
-        // driver.findElement(By.id("_fZl")).click();
 
-        // albo znaleźć jego xpath (czasami nie ma id, albo po prostu łatwiej:
-        //driver.findElement(By.xpath(".//*[@id='_fZl']")).click();
-
-        // ale mogę go też znaleźć po nazwie:
-        driver.findElement(By.name("btnG")).click();
-
-        //driver.quit(); // zakomentowałem bo za szybko działa i od razu zamyka
+        //driver.quit();
     }
 }
 
