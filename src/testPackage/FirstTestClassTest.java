6b84af14746443985351dce29a750cb211630543
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-05-09 23:15:37 +0100

    Section 11 video 76 - próba czy plik zmieni kolor po dodaniu to repo

---
+++ src/testPackage/FirstTestClassTest.java
@@ -0,0 +1,26 @@
+package testPackage;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by olewc on 09/05/2017.
+ */
+public class FirstTestClassTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public static void test1(){
+        System.out.println("jakis test");
+    }
+
+}
