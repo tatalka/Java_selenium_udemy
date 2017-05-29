802e4ad03d03a69412808fbcb02736eae881d58a
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-05-29 23:34:29 +0100

    Section 11 video 77 again - to czego brakowalo w filmiku.

--- src/MyTests/BasicActions.java
+++ src/MyTests/BasicActions.java
@@ -25,9 +25,14 @@ public class BasicActions {
     public void firstTest(){
         driver.get(baseUrl);
         driver.findElement(By.xpath("//div[@id='navbar']//a[@href='/sign_in']")).click();
+        System.out.println("Clicked on login link");
         driver.findElement(By.id("user_email")).clear();
         driver.findElement(By.id("user_email")).sendKeys("poziomka");
-
+        System.out.println("Sending keys to username field");
+        driver.findElement(By.id("user_password")).sendKeys("test");
+        System.out.println("Sending keys to username field");
+        driver.findElement(By.id("user_email")).clear();
+        System.out.println("Clearing the username field");
     }
 
     @After
