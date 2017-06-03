a83aa0d056d74e639ee8662d13ce5101cc94d95d
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-06-03 21:56:42 +0100

    Sekcja 11 Video 82 - tutaj nie było kodu, tylko różnica między schowanym elementem a nieistniejącym

---
+++ src/MyTests/HiddenShownElements.java
@@ -0,0 +1,36 @@
+package MyTests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by olewc on 03/06/2017.
+ */
+public class HiddenShownElements {
+    WebDriver driver;
+    String baseUrl;
+
+    // wyglada na to ze że tutaj nie było żadnych ćwiczeń na kod
+    // chodziło tylko o wytłumaczenie że elementy mogą być cały czas
+    // dostępne, tylko nie pokazane na ekranie => w trybie hidden. cały czas
+    // jest do nich dostęp. 
+
+    @Before
+    public void startUp(){
+
+    }
+
+    @Test
+    public void test01(){
+
+    }
+
+    @After
+    public void tearDown(){
+
+    }
+
+}
+
