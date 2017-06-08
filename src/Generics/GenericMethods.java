0901de630dab68a7581b988575f242534fba0195
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-06-08 23:28:42 +0100

    Sekcja 12 Video 87 - metody generyczne

---
+++ src/Generics/GenericMethods.java
@@ -0,0 +1,32 @@
+package Generics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Created by olewc on 08/06/2017.
+ */
+public class GenericMethods {
+    WebDriver driver;
+
+    public GenericMethods(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public WebElement getElement(String locator, String type){
+        type = type.toLowerCase();
+        if(type.equals("id")){
+            System.out.println("Element found with id : " + locator);// tutaj chyba + locator
+            return this.driver.findElement(By.id(locator));
+        }
+        else if (type.equals("xpath")){
+            System.out.println("Element found with xpath : " + locator);
+            return this.driver.findElement(By.xpath(locator));
+        }
+        else{
+            System.out.println("Locator type not supported");
+            return null;
+        }
+    }
+}
