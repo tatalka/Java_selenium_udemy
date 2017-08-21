1a1d8e6ba7b9043efd531752680374cf727906be
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-08-21 19:56:07 +0100

    Początek frameworka do ogame.

--- src/testPackage/FirstTestClass.java
+++ src/testPackage/FirstTestClass.java
@@ -2,7 +2,6 @@ package testPackage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 
@@ -13,14 +12,16 @@ public class FirstTestClass {
 
     public static void main(String[] args) {
         String baseURL = "http://www.onet.pl";
-        System.setProperty("webdriver.gecko.driver","E:\\developernia\\selenium-java-3.0.1\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver","E:\\developernia\\SeleniumDrivery\\geckodriver.exe");
 //        WebDriver driver = new FirefoxDriver();
 //        driver.get(baseURL);
 //        driver.close();
-        System.setProperty("webdriver.chrome.driver","E:\\developernia\\selenium-java-3.0.1\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","E:\\developernia\\SeleniumDrivery\\chromedriver.exe");
         WebDriver chromeDriver = new ChromeDriver();
 
         chromeDriver.get(baseURL);
+
+        chromeDriver.quit();
         
 
     }
