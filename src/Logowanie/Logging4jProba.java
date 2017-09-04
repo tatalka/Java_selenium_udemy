b43859de852883df19c3892f4988614d03088052
Sebastian Olewczynski <olew@poczta.onet.pl>
2017-09-04 22:25:14 +0100

    sekcja 18 video 120 konfiguracja logowania log4j za pomocą plików properties

--- src/Logowanie/Logging4jProba.java
+++ src/Logowanie/Logging4jProba.java
@@ -1,7 +1,35 @@
 package Logowanie;
 
+import org.apache.log4j.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 /**
  * Created by olewc on 04/09/2017.
  */
 public class Logging4jProba {
+
+    static Logger log = Logger.getLogger(Logging4jProba.class);
+
+    @BeforeClass
+    public static void setUpBeforeClass (){
+        // PropertyConfigurator.configure("F:\\Git_folder\\Java_selenium_udemy\\src\\Logowanie\\log4j.properties");
+        PropertyConfigurator.configure("F:\\Git_folder\\Java_selenium_udemy\\src\\Logowanie\\file_log4j.properties");
+
+
+
+        log.info("running before method");
+    }
+
+    @Test
+    public void test(){
+        log.info("running test method");
+
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass(){
+        log.debug("running after method");
+    }
 }
